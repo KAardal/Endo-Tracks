08caabe72e630fae3786d83a8c2ceb208516ad03
Amanda Koster <amandakoster@gmail.com>
2017-07-10 15:04:53 -0700

    fixing a synch

---
+++ route/user-router.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const {Router} = require('express');
+const s3Uplaod = require('../lib/s3-upload-middleware.js');
+const bearerAuth = require('../lib/bearer-auth-middleware.js');
+const User = require('../model/user.js');
+
+const userRouter = module.expoerts = new Router();
+userRouter.post('/api/users', bearerAuth,)
+
+
+
+// passwordHash: {type: String, required: true},
+// userName: {type: String, required: true, unique: true},
+// email: {type: String, required: true, unique: true},
+// tokenSeed: {type: String, required: true, unique: true},
+// });
