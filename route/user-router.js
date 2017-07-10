faaea8990f4c5141b7fd5c973b26f8dfa69ccd55
Amanda Koster <amandakoster@gmail.com>
2017-07-10 15:53:06 -0700

    built user-router

--- route/user-router.js
+++ route/user-router.js
@@ -1,20 +1,23 @@
-// 'use strict';
-//
-// const {Router} = require('express');
-// const s3Uplaod = require('../lib/s3-upload-middleware.js');
-// const bearerAuth = require('../lib/basic-auth-middleware.js');
-// const User = require('../model/user.js');
-// const jsonParser = require('body-parser');
-//
-// const userRouter = module.exports = new Router();
-// userRouter.POST('/api/users', jsonParser), (req, res, next) => {
-//
-//   console.log();
-// };
-//
-// GET: basic auth
-// passwordHash: {type: String, required: true},
-// userName: {type: String, required: true, unique: true},
-// email: {type: String, required: true, unique: true},
-// tokenSeed: {type: String, required: true, unique: true},
-// });
+'use strict';
+
+const {Router} = require('express');
+const basicAuth = require('../lib/basic-auth-middleware.js');
+const User = require('../model/user.js');
+const jsonParser = require('body-parser');
+
+const userRouter = module.exports = new Router();
+userRouter.post('/api/users', jsonParser, (req, res, next) => {
+  console.log('hit post /api/users');
+
+  User.create(req.body)
+    .then(token => res.send(token))
+    .catch(next);
+});
+
+userRouter.get('/api/users', basicAuth, (req, res, next) => {
+  console.log('hit get /api/users');
+
+  req.user.tokenCreate()
+    .then(token => res.send(token))
+    .catch(next);
+});
