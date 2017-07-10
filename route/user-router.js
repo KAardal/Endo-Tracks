03fb9a0a5966153c7935e0e86890ab5c9991e2cd
Amanda Koster <amandakoster@gmail.com>
2017-07-10 15:41:47 -0700

    incomplete user-route.js

--- route/user-router.js
+++ route/user-router.js
@@ -2,14 +2,17 @@
 //
 // const {Router} = require('express');
 // const s3Uplaod = require('../lib/s3-upload-middleware.js');
-// const bearerAuth = require('../lib/bearer-auth-middleware.js');
+// const bearerAuth = require('../lib/basic-auth-middleware.js');
 // const User = require('../model/user.js');
+// const jsonParser = require('body-parser');
 //
-// const userRouter = module.expoerts = new Router();
-// userRouter.post('/api/users', bearerAuth,)
-
-
-
+// const userRouter = module.exports = new Router();
+// userRouter.POST('/api/users', jsonParser), (req, res, next) => {
+//
+//   console.log();
+// };
+//
+// GET: basic auth
 // passwordHash: {type: String, required: true},
 // userName: {type: String, required: true, unique: true},
 // email: {type: String, required: true, unique: true},
