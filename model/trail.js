22f9ab8f9c25633d964819fbb6811fa06a582f2f
Amanda Koster <amandakoster@gmail.com>
2017-07-11 09:51:15 -0700

    built trails.js

---
+++ model/trail.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const trailSchema = mongoose.Schema ({
+  trailName: {type: String, required: true, unique: true},
+  mapURI: {type: String, required: true, unique: true},
+  difficulty: {type: String, required: true, unique: true},
+  type: {type: String, required: true, unique: true},
+  distance: {type: String, required: true, unique: true},
+  elevation: {type: String, required: true, unique: true},
+  lat: {type: String, required: true, unique: true},
+  long: {type: String, required: true, unique: true},
+  zoom: {type: String, required: true, unique: true},
+  comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'comment' }],
+});
+
+module.exports = mongoose.model('trail', trailSchema);
+
+
+POST;
+GET;
+PUT;
